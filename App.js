cd1a5a130b404141afe9b6c98ad60dd22cc1321f
Jake Walker <jake@dyn3176-247.wlan.ic.ac.uk>
2020-01-09 15:21:41 +0000

    disable warning box for presentation

--- App.js
+++ App.js
@@ -1,45 +1,47 @@
-import React from 'react'
-import * as Font from 'expo-font'
+import React from "react";
+import * as Font from "expo-font";
 
-import { createAppContainer } from 'react-navigation'
-import { createStackNavigator } from 'react-navigation-stack'
-import { fromRight } from 'react-navigation-transitions'
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import { fromRight } from "react-navigation-transitions";
 
-import Login from './pages/Login'
-import ReadQR from './pages/ReadQR'
-import ShowQR from './pages/ShowQR'
+import Login from "./pages/Login";
+import ReadQR from "./pages/ReadQR";
+import ShowQR from "./pages/ShowQR";
 
 /* LOGIN -> READER -> SHOW QR */
 
-const RootStack = createStackNavigator(
-	{
-		Login: Login,
-		ShowQR: ShowQR,
-		ReadQR: ReadQR
-	},
-	{
-		initialRouteName: 'Login',
-		defaultNavigationOptions: {
-			headerTransparent: true
-		},
-		transitionConfig: () => fromRight(500)
-	}
-)
+console.disableYellowBox = true;
 
-const AppContainer = createAppContainer(RootStack)
+const RootStack = createStackNavigator(
+  {
+    Login: Login,
+    ShowQR: ShowQR,
+    ReadQR: ReadQR
+  },
+  {
+    initialRouteName: "Login",
+    defaultNavigationOptions: {
+      headerTransparent: true
+    },
+    transitionConfig: () => fromRight(500)
+  }
+);
+
+const AppContainer = createAppContainer(RootStack);
 
 export default class App extends React.Component {
-	state = {
-		fontLoaded: false
-	}
+  state = {
+    fontLoaded: false
+  };
 
-	async componentDidMount() {
-		await Font.loadAsync({
-			'Poppins-ExtraLight': require('./assets/fonts/Poppins-ExtraLight.ttf')
-		})
-		this.setState({ fontLoaded: true })
-	}
-	render() {
-		return this.state.fontLoaded ? <AppContainer /> : null
-	}
+  async componentDidMount() {
+    await Font.loadAsync({
+      "Poppins-ExtraLight": require("./assets/fonts/Poppins-ExtraLight.ttf")
+    });
+    this.setState({ fontLoaded: true });
+  }
+  render() {
+    return this.state.fontLoaded ? <AppContainer /> : null;
+  }
 }
